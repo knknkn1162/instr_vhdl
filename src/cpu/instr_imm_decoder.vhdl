6b202f27fd762892cf61ce8c6be4684422a21159
knknkn1162 <knknkn1162@gmail.com>
2018-12-25 20:27:24 +0900

    bugfix o_btype_imm in instr_imm_decoder component

--- src/cpu/instr_imm_decoder.vhdl
+++ src/cpu/instr_imm_decoder.vhdl
@@ -15,7 +15,7 @@ architecture behavior of instr_imm_decoder is
 begin
   o_itype_imm <= i_instr(31 downto 20);
   o_stype_imm <= i_instr(31 downto 25) & i_instr(11 downto 7);
-  o_btype_imm <= i_instr(31) & i_instr(7) & i_instr(30 downto 25);
+  o_btype_imm <= i_instr(31) & i_instr(7) & i_instr(30 downto 25) & i_instr(11 downto 8);
   o_utype_imm <= i_instr(31 downto 12);
   o_jtype_imm <= i_instr(31) & i_instr(19 downto 12) & i_instr(20) & i_instr(30 downto 21);
 end architecture;
