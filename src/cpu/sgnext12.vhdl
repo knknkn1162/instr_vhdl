9187b58f5c312e7efe3a7e481660a69384e9f7e4
knknkn1162 <knknkn1162@gmail.com>
2018-12-27 11:56:49 +0900

    fix sgnext12 component

--- src/cpu/sgnext12.vhdl
+++ src/cpu/sgnext12.vhdl
@@ -26,21 +26,25 @@ architecture behavior of sgnext12 is
     );
   end component;
 
-  signal s_btype_imm2 : imm12_vector;
-  signal s_imm12 : imm12_vector;
-  signal s_fill : std_logic_vector(31-CONST_IMM12_SIZE downto 0);
+  signal s_btype_imm2 : std_logic_vector(CONST_IMM12_SIZE downto 0);
+  signal s_itype_imm2 : std_logic_vector(CONST_IMM12_SIZE downto 0);
+  signal s_stype_imm2 : std_logic_vector(CONST_IMM12_SIZE downto 0);
+  signal s_imm13 : std_logic_vector(CONST_IMM12_SIZE downto 0);
+  signal s_fill : std_logic_vector(30-CONST_IMM12_SIZE downto 0);
 begin
-  s_btype_imm2 <= i_btype_imm(11 downto 1) & '0';
-  mux4_0 : mux4 generic map (N=>CONST_IMM12_SIZE)
+  s_btype_imm2 <= i_btype_imm(11 downto 0) & '0';
+  s_itype_imm2 <= i_sgn & i_itype_imm;
+  s_stype_imm2 <= i_sgn & i_stype_imm;
+  mux4_0 : mux4 generic map (N=>CONST_IMM12_SIZE+1)
   port map (
-    i_d00 => i_itype_imm,
-    i_d01 => i_stype_imm,
+    i_d00 => s_itype_imm2,
+    i_d01 => s_stype_imm2,
     i_d10 => s_btype_imm2,
     i_d11 => (others => '0'),
     i_s => i_isb_s,
-    o_y => s_imm12
+    o_y => s_imm13
   );
   s_fill <= (others => i_sgn);
-  o_immext <= s_fill & s_imm12;
+  o_immext <= s_fill & s_imm13;
 
 end architecture;
